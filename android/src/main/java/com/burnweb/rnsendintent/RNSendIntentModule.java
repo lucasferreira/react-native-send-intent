22ae1fdd3a002cb19d428f3b55e652c84a158f8a
andrew.stupchuk <andrii.stupchuk@eleks.com>
2018-01-22 19:49:04 +0200

    fix

--- android/src/main/java/com/burnweb/rnsendintent/RNSendIntentModule.java
+++ android/src/main/java/com/burnweb/rnsendintent/RNSendIntentModule.java
@@ -9,6 +9,8 @@ import android.provider.CalendarContract.Events;
 import android.os.Environment;
 import android.util.Log;
 import android.net.Uri;
+import android.telephony.TelephonyManager;
+import android.content.Context;
 
 import java.util.Map;
 import java.util.HashMap;
