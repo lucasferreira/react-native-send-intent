ff794e01a705da6e6388c83cf6992b319944bda2
Dávid Lévai <david@davidlevai.com>
2018-05-03 15:50:25 +0200

    phoneAppOnly API

--- android/src/main/java/com/burnweb/rnsendintent/RNSendIntentModule.java
+++ android/src/main/java/com/burnweb/rnsendintent/RNSendIntentModule.java
@@ -242,10 +242,13 @@ public class RNSendIntentModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sendPhoneCall(String phoneNumberString) {
+    public void sendPhoneCall(String phoneNumberString, Boolean phoneAppOnly) {
       //Needs permission "android.permission.CALL_PHONE"
       Intent sendIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumberString.replaceAll("#", "%23").trim()));
       sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      if (phoneAppOnly == true) {
+          sendIntent.setPackage("com.android.server.telecom");
+      }
 
       //Check that an app exists to receive the intent
       if (sendIntent.resolveActivity(this.reactContext.getPackageManager()) != null) {
@@ -260,9 +263,12 @@ public class RNSendIntentModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sendPhoneDial(String phoneNumberString) {
+    public void sendPhoneDial(String phoneNumberString, Boolean phoneAppOnly) {
       Intent sendIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumberString.trim()));
       sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      if (phoneAppOnly == true) {
+          sendIntent.setPackage("com.android.server.telecom");
+      }
 
       //Check that an app exists to receive the intent
       if (sendIntent.resolveActivity(this.reactContext.getPackageManager()) != null) {
