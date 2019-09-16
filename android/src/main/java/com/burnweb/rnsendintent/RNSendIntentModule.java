5f46a3999f18663770c91723f2fa3d52a69460a7
Julio Augusto <julioams@icloud.com>
2019-09-16 12:27:17 -0300

    change file profile for suport android x

--- android/src/main/java/com/burnweb/rnsendintent/RNSendIntentModule.java
+++ android/src/main/java/com/burnweb/rnsendintent/RNSendIntentModule.java
@@ -10,7 +10,7 @@ import android.os.Environment;
 import android.util.Log;
 import android.net.Uri;
 import android.os.Build;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 import android.telephony.TelephonyManager;
 import android.content.Context;
 
